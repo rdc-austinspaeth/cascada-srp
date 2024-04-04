fe3a7bbd6b6f9978378d9e090ce55db8c5666ba1
Austin Spaeth <austinspaeth@FLD-VQD3KJ7H77.local>
2024-04-04 11:18:40 -0400

    Finished the nav panels.

--- src/constants/panelContents.ts
+++ src/constants/panelContents.ts
@@ -152,5 +152,324 @@ export const panelContents: any = {
         text: 'Recently sold homes in South Carolina',
       },
     ]
-  }
+  },
+  'Rent': {
+    left: [
+      {
+        elementType: 'headline',
+        text: 'Apartments for rent'
+      },
+      {
+        elementType: 'link',
+        text: 'Apartments for rent in South Carolina',
+      },
+      {
+        elementType: 'link',
+        text: 'Houses for rent in South Carolina',
+      },
+      {
+        elementType: 'link',
+        text: 'All South Carolina rentals',
+      },
+    ],
+    middle: [
+      {
+        elementType: 'headline',
+        text: 'Landlord tools'
+      },
+      {
+        elementType: 'link',
+        text: 'Manage rentals',
+      },
+      {
+        elementType: 'link',
+        text: 'List your rentals',
+      },
+      {
+        elementType: 'link',
+        text: 'Screen tenants',
+      },
+      {
+        elementType: 'link',
+        text: 'Create a lease',
+      },
+      {
+        elementType: 'link',
+        text: 'Collect rent online',
+      },
+    ],
+    right: [
+      {
+        elementType: 'headline',
+        text: 'Home renting tips'
+      },
+      {
+        elementType: 'link',
+        text: 'Renter\'s resource center',
+      },
+      {
+        elementType: 'link',
+        text: 'Should I rent or buy?',
+      },
+      {
+        elementType: 'link',
+        text: 'Debunked! 8 myths about renting you should stop believing immediately',
+      },
+      {
+        elementType: 'line',
+      },
+      {
+        elementType: 'link',
+        text: 'More rentin insights',
+      },
+    ]
+  },
+  'Mortgage': {
+    left: [
+      {
+        elementType: 'headline',
+        text: 'Mortgage rates'
+      },
+      {
+        elementType: 'link',
+        text: 'Get pre-approved',
+      },
+      {
+        elementType: 'link',
+        text: 'South Carolina mortgage rates',
+      },
+      {
+        elementType: 'link',
+        text: 'Home equity financing rates',
+      },
+      {
+        elementType: 'link',
+        text: 'Refinance rates',
+      },
+      {
+        elementType: 'link',
+        text: 'Finance advice',
+      },
+      {
+        elementType: 'link',
+        text: 'For veterans',
+      },
+    ],
+    middle: [
+      {
+        elementType: 'headline',
+        text: 'Calculators'
+      },
+      {
+        elementType: 'link',
+        text: 'Mortgage calculator',
+      },
+      {
+        elementType: 'link',
+        text: 'Refinance calculator',
+      },
+      {
+        elementType: 'link',
+        text: 'How much house can I afford',
+      },
+      {
+        elementType: 'link',
+        text: 'Rent vs. buy',
+      },
+    ],
+    right: [
+      {
+        elementType: 'headline',
+        text: 'Financial advice'
+      },
+      {
+        elementType: 'link',
+        text: '6 ways home buyers mess up getting a mortgage',
+      },
+      {
+        elementType: 'link',
+        text: 'Mortgage guide',
+      },
+      {
+        elementType: 'link',
+        text: 'Learn about home insurance',
+      },
+      {
+        elementType: 'link',
+        text: 'More finance insights',
+      },
+    ]
+  },
+  'Find Realtors<sup>®</sup>': {
+    left: [
+      {
+        elementType: 'headline',
+        text: 'Find REALTORS'
+      },
+      {
+        elementType: 'link',
+        text: 'Search for real estate agent',
+      },
+    ],
+    middle: [
+      {
+        elementType: 'headline',
+        text: 'Why use a REALTOR'
+      },
+      {
+        elementType: 'link',
+        text: '6 reasons you should never buy or sell a home without an agent',
+      },
+      {
+        elementType: 'link',
+        text: 'Difference between agent, broker & REALTOR',
+      },
+      {
+        elementType: 'link',
+        text: 'Listing vs. buyer agent',
+      },
+      {
+        elementType: 'link',
+        text: 'How to find a REALTOR',
+      },
+    ],
+    right: [
+      {
+        elementType: 'headline',
+        text: 'News around REALTORS'
+      },
+      {
+        elementType: 'link',
+        text: 'Real estate agents reveal the toughest home buyers they\'ve ever met',
+      },
+      {
+        elementType: 'link',
+        text: 'More news around REALTORS',
+      },
+    ]
+  },
+  'News & Insights': {
+    left: [
+      {
+        elementType: 'headline',
+        text: 'News'
+      },
+      {
+        elementType: 'link',
+        text: 'The latest news',
+      },
+      {
+        elementType: 'link',
+        text: 'Housing trends',
+      },
+      {
+        elementType: 'link',
+        text: 'Real estate news',
+      },
+      {
+        elementType: 'link',
+        text: 'Celebrity real estate',
+      },
+      {
+        elementType: 'link',
+        text: 'Unique homes',
+      },
+      {
+        elementType: 'link',
+        text: 'For pros',
+      },
+      {
+        elementType: 'link',
+        text: 'Corporate blog',
+      },
+    ],
+    middle: [
+      {
+        elementType: 'headline',
+        text: 'Insights'
+      },
+      {
+        elementType: 'link',
+        text: 'Buying',
+      },
+      {
+        elementType: 'link',
+        text: 'Selling',
+      },
+      {
+        elementType: 'link',
+        text: 'Renting',
+      },
+      {
+        elementType: 'link',
+        text: 'Financing',
+      },
+      {
+        elementType: 'link',
+        text: 'Moving',
+      },
+      {
+        elementType: 'link',
+        text: 'Living',
+      },
+      {
+        elementType: 'link',
+        text: 'Home improvement',
+      },
+      {
+        elementType: 'link',
+        text: 'Research',
+      },
+      {
+        elementType: 'link',
+        text: '2024 housing market predictions',
+      },
+      {
+        elementType: 'link',
+        text: '2023 hottest zip codes',
+      },
+    ],
+    right: [
+      {
+        elementType: 'headline',
+        text: 'Guides & more'
+      },
+      {
+        elementType: 'link',
+        text: 'All guides',
+      },
+      {
+        elementType: 'link',
+        text: 'Complete guide on how to sell your home',
+      },
+      {
+        elementType: 'link',
+        text: 'First-time home buyer resource center',
+      },
+      {
+        elementType: 'link',
+        text: 'Mortgage guide',
+      },
+      {
+        elementType: 'link',
+        text: 'Veterans home buyer guide',
+      },
+      {
+        elementType: 'link',
+        text: 'USDA home loan guide',
+      },
+      {
+        elementType: 'link',
+        text: 'Home insurance guide',
+      },
+      {
+        elementType: 'link',
+        text: 'Real estate videos',
+      },
+      {
+        elementType: 'link',
+        text: 'Housing resources',
+      },
+    ],
+  },
 }
