79a0a64142a6dfc5a7cde47c9f01d9b3d9227c97
Victor Cho <victor.cho@move.com>
2024-04-05 01:54:49 -0700

    Implement AFS Ad

--- src/components/FooterAd/FooterAd.tsx
+++ src/components/FooterAd/FooterAd.tsx
@@ -1,15 +1,22 @@
 import React from "react"
 import { footerAdPlacholder } from "./FooterAd.css"
+import { SrpAFSAd } from '@moveinc/rdc-ads-media';
 
 export const FooterAd: React.FunctionComponent = () => {
+    const page = 1;
   return (
     <React.Fragment>
-      <div className={ footerAdPlacholder }>
-        Advertisement
+      <div className={footerAdPlacholder}>
+        <div id="div-gpt-ad-SRPFOOT18LIST"></div>
       </div>
-      <div style={{ height: 350 }} className={ footerAdPlacholder }>
-        Advertisement
+      <div>
+        <SrpAFSAd
+          isListView={true}
+          currentPage={page}
+          queryVariables={null}
+          containerId="afscontainer1"
+        />
       </div>
     </React.Fragment>
-  )
+  );
 }
